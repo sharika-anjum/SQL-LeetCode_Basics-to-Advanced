611c21746f983871b76cc62d5a6241f90ae5d9e6
Sharika Anjum Mondal <sharikaanjumm@gmail.com>
2024-05-11 15:51:33 +0530

    Delete 1164-product-price-at-a-given-date directory

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++
@@ -1,7 +0,0 @@
-# Write your MySQL query statement below
-Select product_id,new_price as price from (Select *, row_number() over(partition by product_id order by change_date desc) as rn from Products
-where change_date<='2019-08-16') b where rn=1
-UNION
-Select distinct product_id, 10 as price from Products
-where product_id not in (Select distinct product_id from Products
-where change_date<='2019-08-16')
